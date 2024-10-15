32f5d32e288506bf74db433195c802a6c731ee24
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-10-15 22:49:00 +0700

    💫 Designing Login Component (Done)

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { Button, Flex, Image, Space, Typography } from "antd";
+import Lottie from "lottie-react";
+import animationData from "../lotties/LoginAnimation.json";
+import logoBlack from "../assets/logoBlack.svg";
 
 const Login = () => {
-  return <div>Login</div>;
+  const { Title } = Typography;
+
+  return (
+    <Flex justify="center" align="center">
+      <div
+        style={{
+          backgroundColor: "#e6e4e4",
+          margin: "8%",
+          padding: "2%",
+          borderRadius: "20px",
+        }}>
+        <Flex gap="middle" justify="center" align="center">
+          <Lottie animationData={animationData} />
+          <Flex vertical align="center">
+            <Space size="middle" direction="vertical">
+              <Image
+                width={200}
+                preview={false}
+                src={logoBlack}
+                style={{ marginLeft: "40%" }}
+              />
+              <Title level={4}>
+                Connect Anytime,{" "}
+                <span style={{ color: "#58ABDF" }}>Without Boundaries</span>
+              </Title>
+              <Button type="primary" block size="large">
+                Login With Google
+              </Button>
+            </Space>
+          </Flex>
+        </Flex>
+      </div>
+    </Flex>
+  );
 };
 
 export default Login;
