0468de9bd0383fad5ae7dbda263cbe9fb4afbb80
NyanLinHtoo <nyanlinhtoo.ygn@gmail.com>
2024-10-17 00:17:06 +0700

    ⚡️ add breadCrumbs in header component (done)

--- src/components/BreadCrumbs.tsx
+++ src/components/BreadCrumbs.tsx
@@ -1,5 +1,20 @@
-const BreadCrums = () => {
-  return <div>BreadCrums</div>;
+import { Breadcrumb, Flex, Tag } from "antd";
+
+interface breadCrumbsItems {
+  title: string;
+}
+interface BreadCrumbsProps {
+  breadCrumbs: breadCrumbsItems[];
+}
+
+const BreadCrumbs = ({ breadCrumbs }: BreadCrumbsProps) => {
+  return (
+    <Flex align="center" style={{ height: "8vh", marginLeft: "2.5%" }}>
+      <Tag>
+        <Breadcrumb items={breadCrumbs} />
+      </Tag>
+    </Flex>
+  );
 };
 
-export default BreadCrums;
+export default BreadCrumbs;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
-import { Button, Flex, Image, Tag, Typography } from "antd";
+import { Button, Divider, Flex, Image, Tag, Typography } from "antd";
 import TSlogo from "../assets/TS-short-logo.svg";
 import { useAppSelector } from "../app/hook";
 import { Link } from "react-router-dom";
 import { LogoutOutlined } from "@ant-design/icons";
 import useAuth from "../hooks/useAuth";
+import BreadCrumbs from "./BreadCrumbs";
+import { useState } from "react";
 
 const Header = () => {
   const { Text } = Typography;
@@ -11,42 +13,56 @@ const Header = () => {
 
   const username = useAppSelector((state) => state.auth.userInfo?.name);
 
+  const [breadCrumbs, setBreadCrumbs] = useState([
+    {
+      title: "Dashboard",
+    },
+    {
+      title: "Dashboard",
+    },
+  ]);
+
   return (
-    <Flex
-      gap="middle"
-      align="center"
-      justify="space-between"
-      style={{ height: "8vh" }}>
-      <Link to="/">
-        <Image
-          width={60}
-          src={TSlogo}
-          preview={false}
-          style={{ marginLeft: "50%" }}
-        />
-      </Link>
-      {username && (
-        <Text
-          strong
-          style={{
-            fontSize: 20,
-          }}>
-          Hello,{" "}
-          <Tag
-            color="blue"
+    <>
+      <Flex
+        gap="middle"
+        align="center"
+        justify="space-between"
+        style={{ height: "8vh" }}>
+        <Link to="/">
+          <Image
+            width={60}
+            src={TSlogo}
+            preview={false}
+            style={{ marginLeft: "50%" }}
+          />
+        </Link>
+        {username && (
+          <Text
+            strong
             style={{
-              fontSize: 15,
+              fontSize: 20,
             }}>
-            {username}
-          </Tag>
-        </Text>
-      )}
-      <Button
-        icon={<LogoutOutlined />}
-        style={{ marginRight: "1%" }}
-        onClick={logout}
-      />
-    </Flex>
+            Hello,{" "}
+            <Tag
+              color="blue"
+              style={{
+                fontSize: 15,
+              }}>
+              {username}
+            </Tag>
+          </Text>
+        )}
+        <Button
+          icon={<LogoutOutlined />}
+          style={{ marginRight: "1%" }}
+          onClick={logout}
+        />
+      </Flex>
+      <Divider />
+      <BreadCrumbs breadCrumbs={breadCrumbs} />
+      <Divider />
+    </>
   );
 };
 
